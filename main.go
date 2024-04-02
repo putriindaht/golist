c27cdc32e546661b3a3956d57df421d2238aa05e
putriindaht <putriindaht@gmail.com>
2024-04-02 09:43:56 +0700

    simple crud

--- main.go
+++ main.go
@@ -1,30 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var todolist = []string{}
+
+// Create a new scanner to read from standard input
+var scanner = bufio.NewScanner(os.Stdin)
+
 func main() {
-	var command string
-	fmt.Println("Welcome to Golist!")
-	fmt.Println("Simple Go App To Do List")
-	fmt.Println("Type 'help' to see the command list")
-	fmt.Scan(&command)
+	for {
+		var command string
+		greeting()
+		fmt.Println("Type the command or type 'help' to see the command list")
+		fmt.Scan(&command)
 
-	if command == "help" {
-		getListCommand()
+		switch command {
+		case "help":
+			getListCommand()
+		case "add":
+			addToDoList()
+		case "delete":
+			deleteToDoList()
+		case "update":
+			updateToDoList()
+		default:
+			fmt.Println("Yor command is not available, check the available command list with type 'help'")
+		}
 	}
-
-	todolist := []string{}
-	var newList string
-	fmt.Println("Add new list:")
-	fmt.Scan(&newList)
-
-	todolist = append(todolist, newList)
-	fmt.Printf("All to do list: %v", todolist)
 
 }
 
+func greeting() {
+	fmt.Println("\nWelcome to Golist!")
+	fmt.Println("Simple Go App To Do List")
+	fmt.Println("Your to do list:")
+	for index, value := range todolist {
+		fmt.Printf("%d. %s\n", index+1, value)
+	}
+}
+
 func getListCommand() {
-	fmt.Println("Hi")
+	commandList := "The commands are: \nhelp: get all commands \nadd: add new list  \ndelete: delete to do list \nupdate: update to do list"
+	fmt.Println(commandList)
+
+}
+
+func addToDoList() {
+	fmt.Println("Add new list:")
+	scanner.Scan()
+	newList := scanner.Text()
+
+	todolist = append(todolist, newList)
+}
+
+func deleteToDoList() {
+	var number uint
+	fmt.Println("Type the number you want to delete:")
+	fmt.Scan(&number)
+	index := number - 1
+	todolist = append(todolist[:index], todolist[index+1:]...)
+	fmt.Println("todolist after deletion:", todolist)
+
+}
+
+func updateToDoList() {
+	var number uint
+	fmt.Println("Type the number you want to update:")
+	fmt.Scan(&number)
+	fmt.Println("Type the new value:")
+	scanner.Scan()
+	newValue := scanner.Text()
+	index := number - 1
+	todolist[index] = newValue
+	fmt.Println("Updated todolist:", todolist)
 }
